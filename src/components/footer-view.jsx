ecc44c06f34218fc51cc1f3f0bb751fc774d8bd7
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-09-23 08:05:39 -0400

    Update footer-view.jsx

--- src/components/footer-view.jsx
+++ src/components/footer-view.jsx
@@ -4,8 +4,8 @@ import linkedin from "./images/linkedin-svgrepo-com.svg";
 export const FooterView = () => {
     return (
         <div className="footer">
-            <a href="#" target="_blank" className="footerItem"><img src={github} height="50px"></img></a>
-            <a href="#" target="_blank" className="footerItem"><img src={linkedin} height="50px"></img></a>
+            <a href="https://github.com/SethSingletary" target="_blank" className="footerItem"><img src={github} height="50px"></img></a>
+            <a href="https://www.linkedin.com/in/seth-singletary" target="_blank" className="footerItem"><img src={linkedin} height="50px"></img></a>
         </div>
     )
 }
